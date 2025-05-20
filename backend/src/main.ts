cc1ccc40af9bd2d1e2777cc04e31bb3af3cd2d85
Janier Chara Cardona <jchara08@gmail.com>
2025-05-19 19:44:40 -0500

    Agregar archivo de ejemplo de configuración y actualizar Docker Compose para usar variables de entorno
    - Se creó el archivo .env.example con configuraciones de base de datos PostgreSQL.
    - Se modificó docker-compose.yml para utilizar variables de entorno en lugar de valores fijos.
    - Se mejoró el archivo main.ts para habilitar CORS y registrar el puerto en el que se ejecuta el servidor.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  await app.listen(port);
+  console.log(`Server is running on port ${port}`);
 }
 bootstrap();
