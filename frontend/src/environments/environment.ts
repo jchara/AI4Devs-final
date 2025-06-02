b4d16e6467fac2382fc1ce8c03ede80b97f0a63c
Janier Chara Cardona <jchara08@gmail.com>
2025-06-01 23:55:13 -0500

    feat: Reestructurar archivos de configuración de entornos
    - Se ha eliminado el archivo `environment.development.ts` y se ha creado `environment.prod.ts` para la configuración de producción.
    - Se ha actualizado el archivo `environment.ts` para reflejar la configuración de desarrollo, cambiando la URL de la API y el estado de producción.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://api.tudominio.com',
-  name: 'production',
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  name: 'development',
 };
